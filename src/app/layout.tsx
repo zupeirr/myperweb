ac35df54c13324bcb1e7cc40891815873f60468a
zupeirr <subeersulub10@gmail.com>
2025-06-27 19:56:34 +0300

    layout update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from '@/components/theme-provider';
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
 export const metadata: Metadata = {
   title: 'Suber Sulub | Creative Full-Stack Developer',
   description: 'A personal portfolio for showcasing projects and skills.',
@@ -26,6 +26,7 @@ export default function RootLayout({
           {children}
           <Toaster />
         </ThemeProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
